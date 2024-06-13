789eb5694d792f8b023dac3dbf994cf12a6517d2
landerson98n <landerson98n@gmail.com>
2024-06-13 08:32:14 -0300

    railway

--- zumbi/settings.py
+++ zumbi/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,8 +85,12 @@ WSGI_APPLICATION = 'zumbi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("PGDATABASE", default=""),
+        'USER': os.getenv("PGUSER", default=""),
+        'PASSWORD': os.getenv("PGPASSWORD", default=""),
+        'HOST': os.getenv("PGHOST", default=""),
+        'PORT': os.getenv("PGPORT", default=""),
     }
 }
 
